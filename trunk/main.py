a181a751f65446bae8f6c903148f01c92fa48f35
dan.egnor@gmail.com <dan.egnor@gmail.com@35f1891e-c0e9-cb00-e6c5-4f2760a22bef>
2011-12-31 03:05:38 +0000

    Feedback form, basic standings

--- trunk/main.py
+++ trunk/main.py
@@ -12,8 +12,23 @@ class MainPage(webapp.RequestHandler):
   def get(self):
     game = model.GetGame()
     props = { "game": model.GetProperties(game), "teams": [] }
-    for team in model.Team.all().ancestor(game).order("name"):
-      props["teams"].append(model.GetProperties(team))
+    props_by_team = {}
+    for team in model.Team.all().ancestor(game):
+      team_props = props_by_team[team.key()] = model.GetProperties(team)
+      team_props["solves"] = set()
+      team_props["solve_time"] = 0
+      props["teams"].append(team_props)
+
+    for guess in model.Guess.all().ancestor(game).order("timestamp"):
+      if guess.answer in guess.parent().answers:
+        team_props = props_by_team[guess.team.key()]
+        if guess.parent().key() not in team_props["solves"]:
+          team_props["solves"].add(guess.parent().key())
+          team_props["solve_time"] = guess.timestamp
+
+    props["teams"].sort(key = lambda tp:
+        (-len(tp["solves"]), tp["solve_time"], tp["name"]))
+
     self.response.out.write(template.render("main.dj.html", props))
 
 app = webapp.WSGIApplication([('/', MainPage)], debug=True)
