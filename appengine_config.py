0e525d7d276712a09c0030f8de15ce275a08ac97
dan.egnor@gmail.com <dan.egnor@gmail.com@35f1891e-c0e9-cb00-e6c5-4f2760a22bef>
2012-03-01 05:52:22 +0000

    add appengine config for django version

---
+++ appengine_config.py
@@ -0,0 +1,6 @@
+# App Engine configuration
+# http://code.google.com/p/googleappengine/issues/detail?id=4489
+# http://stackoverflow.com/questions/4783974/appengine-config-py-in-google-app-engine
+# http://groups.google.com/group/google-appengine-python/browse_thread/thread/59f788fe466417de/0970767831566554
+
+webapp_django_version = "1.2"
